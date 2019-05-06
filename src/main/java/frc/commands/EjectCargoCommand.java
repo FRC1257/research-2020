48552fe56bf6270b08576a99f74c912935f746b2
tony234423 <45372463+tony234423@users.noreply.github.com>
2019-05-07 05:15:02 +0800

    Create EjectCargoCommand.java

---
+++ src/main/java/frc/commands/EjectCargoCommand.java
@@ -0,0 +1,41 @@
+package frc.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+import frc.robot.Robot;
+import frc.subsystems.*;
+
+/**
+ * Eject a cargo ball
+ */
+
+public class EjectCargoCommand extends Command {
+
+    private CargoRoller cargoIntake;
+
+    public EjectCargoCommand() {
+        cargoIntake = Robot.cargoRoller;
+
+        requires(cargoIntake);
+    }
+
+    @Override
+    public void execute() {
+        cargoIntake.eject();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    public void end() {
+        cargoIntake.neutral();
+    }
+
+    @Override
+    public void interrupted() {
+        end();
+    }
+}
