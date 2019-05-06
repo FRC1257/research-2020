a4a1d9cb43307059f741d768bf69e477c5d82158
tony234423 <45372463+tony234423@users.noreply.github.com>
2019-05-07 05:11:52 +0800

    Update Drive.java

--- src/main/java/frc/commands/Drive.java
+++ src/main/java/frc/commands/Drive.java
@@ -1,46 +1,52 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 
 import frc.robot.Robot;
+import frc.robot.OI;
+import frc.subsystems.Drivetrain;
 
-public class Drive extends Command {
-  public Drive() {
-    requires(Robot.driveTrain);
-  }
+/**
+ * Set the speed of the drivetrain with the values from the controller
+ *
+ * Default command of the Drivetrain subsystem
+ */
 
-  // Called just before this Command runs the first time
-  @Override
-  protected void initialize() {
-  }
+public class DriveCommand extends Command {
 
-  // Called repeatedly when this Command is scheduled to run
-  @Override
-  protected void execute() {
-    Robot.driveTrain.drive(Robot.oi.getDriveForwardSpeed(), Robot.oi.getDriveTurnSpeed());
-  }
+    private Drivetrain drivetrain;
+    private OI oi;
 
-  // Make this return true when this Command no longer needs to run execute()
-  @Override
-  protected boolean isFinished() {
-    return false;
-  }
+    public DriveCommand() {
+        drivetrain = Robot.drivetrain;
+        oi = Robot.oi;
 
-  // Called once after isFinished returns true
-  @Override
-  protected void end() {
-  }
+        requires(drivetrain);
+    }
 
-  // Called when another command which requires one or more of the same
-  // subsystems is scheduled to run
-  @Override
-  protected void interrupted() {
-  }
+    @Override
+    public void execute() {
+        if(drivetrain.getCheeseDrive() == false){
+        drivetrain.drive(oi.getDriveForwardSpeed(), oi.getDriveTurnSpeed());
+        }
+        if(drivetrain.getCheeseDrive == true){
+        drivetrain.drive(oi.getDriveForwardSpeed(), oi.getDriveCurvature());
+        }
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    public void end() {
+        
+    }
+
+    @Override
+    public void interrupted() {
+        end();
+    }
 }
+
