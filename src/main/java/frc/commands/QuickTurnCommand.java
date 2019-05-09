a843345260e664f5fdeebd0ddfb73f7a059c218a
tony234423 <45372463+tony234423@users.noreply.github.com>
2019-05-10 03:26:03 +0800

    fixed import

--- src/main/java/frc/commands/QuickTurnCommand.java
+++ src/main/java/frc/commands/QuickTurnCommand.java
@@ -3,7 +3,7 @@ package frc.commands;
 import edu.wpi.first.wpilibj.command.InstantCommand;
 
 import frc.robot.Robot;
-import frc.robot.subsystems.Drivetrain;
+import frc.subsystems.Drivetrain;
 
 
 public class QuickTurnCommand extends InstantCommand {
