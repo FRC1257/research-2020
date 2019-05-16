aa1031d1147db63101ddac130bf5aad2eb7ed191
tony234423 <45372463+tony234423@users.noreply.github.com>
2019-05-17 06:43:14 +0800

    Create NeutralHatchCommand.java

---
+++ src/main/java/frc/commands/NeutralHatchCommand.java
@@ -0,0 +1,43 @@
+package frc.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+import frc.robot.Robot;
+import frc.subsystems.HatchIntake;
+
+/**
+ * Constantly intake hatch panel in order to retain it
+ *
+ * Default command of HatchIntake subsystem
+ */
+
+public class NeutralHatchCommand extends Command {
+
+    private HatchIntake hatchIntake;
+
+    public NeutralHatchCommand() {
+        hatchIntake = Robot.hatchIntake;
+
+        requires(hatchIntake);
+    }
+
+    @Override
+    public void execute() {
+        hatchIntake.neutral();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    public void end() {
+        
+    }
+
+    @Override
+    public void interrupted() {
+        end();
+    }
+}
