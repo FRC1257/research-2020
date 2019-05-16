abcbfd6605f0a18b0ff508e9ea9217407e588e20
tony234423 <45372463+tony234423@users.noreply.github.com>
2019-05-17 06:25:45 +0800

    Create NeutralHatchCommand.java

---
+++ src/main/java/frc/commands/NeutralHatchCommand.java
@@ -0,0 +1,43 @@
+package frc.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+import frc.robot.Robot;
+import frc.robot.subsystems.cargointake.CargoRoller;
+
+/**
+ * Constantly intake cargo ball in order to retain it
+ *
+ * Default command of CargoRoller subsystem
+ */
+
+public class NeutralCargoCommand extends Command {
+
+    private CargoRoller cargoIntake;
+
+    public NeutralCargoCommand() {
+        cargoIntake = Robot.cargoRoller;
+
+        requires(cargoIntake);
+    }
+
+    @Override
+    public void execute() {
+        cargoIntake.neutral();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    public void end() {
+        
+    }
+
+    @Override
+    public void interrupted() {
+        end();
+    }
+}
