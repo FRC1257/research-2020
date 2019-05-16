37a2e098d0d015a4af87b9850122fc9c70388a42
tony234423 <45372463+tony234423@users.noreply.github.com>
2019-05-17 04:10:08 +0800

    Update Gyro.java

--- src/main/java/frc/util/Gyro.java
+++ src/main/java/frc/util/Gyro.java
@@ -2,64 +2,146 @@ package frc.util;
 
 import com.kauailabs.navx.frc.*;
 
-import edu.wpi.first.wpilibj.I2C.Port;
-import edu.wpi.first.wpilibj.smartdashboard.*;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.SPI.Port;
 
 /**
- * <h1>Gyro</h1>
- * The Gyro gets us our Yaw angle, in degrees. Our "0" point is the Yaw at the start of the match.
- * @author Allen Du
- * @since 2019-01-23
+ * Wrapper class for the navX-MXP and ADXRS450_Gyro
+ * Mostly utilizes the navX-MXP, but uses the ADXRS450 as backup if it disconnects
  */
-public class Gyro {
-    public AHRS navx;
-    private static Gyro instance = null;
-    private String angle = "Yaw angle: ";
 
-    /**
-     * Constructs a Gyro object.
-     */
+public class Gyro {
+
+    private static Gyro instance = null;
+
+    private AHRS navx;
+    private double resetRoll;
+    private double resetPitch;
+
+    private ADXRS450_Gyro gyro;
+
     private Gyro() {
         navx = new AHRS(Port.kMXP);
+        resetRoll = 0;
+        resetPitch = 0;
+
+        gyro = new ADXRS450_Gyro();
+        gyro.calibrate();
     }
 
     /**
-     * Singleton.
-     * @return A Gyro object.
+     * Gets the current yaw angle.
+     * 
+     * @return The angle in degrees.
      */
+    public double getYawAngle() {
+        return navx.getYaw();
+    }
+
+    /**
+     * Gets the current roll angle.
+     * 
+     * @return The angle in degrees.
+     */
+    public double getRollAngle() {
+        return navx.getRoll() - resetRoll;
+    }
+
+    /**
+     * Gets the current pitch angle.
+     * 
+     * @return The angle in degrees.
+     */
+    public double getPitchAngle() {
+        return navx.getPitch() - resetPitch;
+    }
+    /**
+     * Sets the current yaw angle to "0".
+     */
+    public void zeroYawAngle() {
+        navx.zeroYaw();
+    }
+
+    /**
+     * Sets the current roll angle to "0".
+     */
+    public void zeroRollAngle() {
+        resetRoll = getRollAngle();
+    }
+
+    /**
+     * Sets the current pitch angle to "0".
+     */
+    public void zeroPitchAngle() {
+        resetPitch = getPitchAngle();
+    }
+
+    /**
+     * Gets the current rotation of the robot.
+     * 
+     * @return The angle in degrees.
+     */
+    public double getRobotAngle() {
+        return getYawAngle();
+    }
+
+    /**
+     * Gets the current tilt of the robot while climbing.
+     * Utilizes the NavX if it is connected, but otherwise it will use the backup gyro
+     * 
+     * @return THe angle in degrees
+     */
+    public double getClimbTiltAngle() {
+        if(navXConnected()) {
+            return getRollAngle();
+        }
+        else {
+            return -gyro.getAngle();
+        }
+    }
+
+    /**
+     * Sets the current rotation of the robot to "0".
+     */
+    public void zeroRobotAngle() {
+        zeroYawAngle();
+    }
+
+    /**
+     * Sets the current tilt of the robot to "0".
+     */
+    public void zeroClimbTiltAngle() {
+        zeroRollAngle();
+        gyro.reset();
+    }
+
+    /**
+     * Returns whether or not the NavX is currently connected and sending valid data 
+     * 
+     * @return whether or not the NavX is currently connected
+     */
+    public boolean navXConnected() {
+        return navx.isConnected();
+    }
+
+    /**
+     * Displays the angles on {@code SmartDashboard}.
+     */
+    public void outputValues() {
+        SmartDashboard.putNumber("Yaw Angle", getYawAngle());
+        SmartDashboard.putNumber("Roll Angle", getRollAngle());
+        SmartDashboard.putNumber("Pitch Angle", getPitchAngle());
+
+        SmartDashboard.putNumber("Robot Angle", getRobotAngle());
+        SmartDashboard.putNumber("Climb Tilt Angle", getClimbTiltAngle());
+        SmartDashboard.putBoolean("Gyro Connected", navXConnected());
+    }
+
     public static Gyro getInstance() {
         if (instance == null) {
             instance = new Gyro();
         }
         return instance;
-    }
-
-    /**
-     * Sets the current Yaw angle to "0".
-     */
-    public void zeroAngle() {
-        navx.zeroYaw();
-    }
-
-    /**
-     * Gets the current Yaw angle.
-     * @return The angle in degrees.
-     */
-    public double getAngle() {
-        return navx.getYaw();
-    }
-
-    /**
-     * Resets the Gyro.
-     */
-    public void resetAngle() {
-        navx.reset();
-    }
-
-    /**
-     * Displays the Yaw angle on {@code SmartDashboard}.
-     */
-    public void displayAngle() {
-        SmartDashboard.putNumber(angle, getAngle());
     }
 }
