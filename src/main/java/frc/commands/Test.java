b93d9b5c0a4eba82f261b9b9c9df54a21ca80552
tony234423 <45372463+tony234423@users.noreply.github.com>
2019-05-17 06:44:51 +0800

    Update Test.java

--- src/main/java/frc/commands/Test.java
+++ src/main/java/frc/commands/Test.java
@@ -9,23 +9,27 @@ public class Test extends CommandGroup{
     public Test(){
        
         addSequential(new Drive(0.5, 0) , 1);
-        addSequential(new Progress());
+        addSequential(new Nothing());
         addSequential(new Drive(-0.5, 0) , 1);
-        addSequential(new Progress());
+        addSequential(new Nothing());
         addSequential(new TurnLeftCommand(), 1);
-        addSequential(new Progress());
+        addSequential(new Nothing());
         addSequential(new TurnRightCommand(), 1);
-        addSequential(new Progress());
+        addSequential(new Nothing());
         addSequential(new EjectCargoCommand(), 1);
-        addSequential(new Progress());
+        addSequential(new Nothing());
         addSequential(new IntakeCargoCommand(), 1);
-        addSequential(new Progress());
+        addSequential(new Nothing());
+        addSequential(new EjectHatchCommand(),1);
+        addSequential(new Nothing());
+        addSequential(new IntakeHatchCommand,1);
+        addSequential(new Nothing());
         addSequential(new AdvanceClimbCommand());
         addSequential(new Nothing(), 5);
-        addSequential(new Progress());
+        addSequential(new Nothing());
         addSequential(new AdvanceClimbCommand()); // when this happens make sure that the robot doesnt die
         addSequential(new Nothing(), 5);
-        addSequential(new Progress());
+        addSequential(new Nothing());
         addSequential(new AdvanceClimbCommand());
     }
     public void end() {
