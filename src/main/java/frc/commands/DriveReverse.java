9f3b857e219b964b6757565810363098261da246
tony234423 <45372463+tony234423@users.noreply.github.com>
2019-05-17 10:42:22 -0400

    Create DriveReverse.java

---
+++ src/main/java/frc/commands/DriveReverse.java
@@ -0,0 +1,35 @@
+package frc.commands;
+
+import edu.wpi.first.wpilibj.command.InstantCommand;
+
+import frc.robot.Robot;
+import frc.subsystems.Drivetrain;
+
+
+
+public class DriveReverse extends InstantCommand {
+
+    private Drivetrain drivetrain;
+
+    public DriveReverse() {
+        super();
+        
+        drivetrain = Robot.drivetrain;
+        requires(drivetrain);
+    }
+
+    @Override
+    public void initialize() {
+        drivetrain.toggleReverse();
+    }
+
+    @Override
+    public void end() {
+        
+    }
+
+    @Override
+    public void interrupted() {
+        end();
+    }
+}
