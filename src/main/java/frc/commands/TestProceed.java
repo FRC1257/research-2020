69e45df3876b6edc3b1b23295b956d2bace583e7
tony234423 <45372463+tony234423@users.noreply.github.com>
2019-05-22 08:20:23 +0800

    Update TestProceed.java

--- src/main/java/frc/commands/TestProceed.java
+++ src/main/java/frc/commands/TestProceed.java
@@ -1,24 +1,17 @@
 package frc.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
-import frc.robot.Robot;
-import frc.robot.OI;
 
 
 public class TestProceed extends Command {
     //proceed and wait command
-    OI oi;
 
     public TestProceed() {
         super();
-        oi = Robot.oi;
     }
 
     @Override
     public void execute() {
-        if(oi.testProceed()){
-            end();
-        }
     }
 
     @Override
