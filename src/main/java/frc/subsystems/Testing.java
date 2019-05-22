502a9f7549fecd6ee21c3cd9010e683f5de75799
tony234423 <45372463+tony234423@users.noreply.github.com>
2019-05-22 08:27:04 +0800

    Create Testing.java

---
+++ src/main/java/frc/subsystems/Testing.java
@@ -0,0 +1,23 @@
+package frc.subsystems;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.commands.*;
+
+
+
+public class Testing extends Subsystem {
+    boolean proceed;
+
+    public Testing(){
+    proceed = false;
+    
+    }
+
+    @Override
+    public void initDefaultCommand() {
+        //no
+    }
+    public void Proceed() {
+    proceed = true;
+    }
+    
+    }
