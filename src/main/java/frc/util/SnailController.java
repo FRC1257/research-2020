b285054b91650c00208a29172ddaf0bc9955cf02
tony234423 <45372463+tony234423@users.noreply.github.com>
2019-06-08 19:45:23 +0800

    Update SnailController.java

--- src/main/java/frc/util/SnailController.java
+++ src/main/java/frc/util/SnailController.java
@@ -5,9 +5,10 @@ import edu.wpi.first.wpilibj.XboxController;
 // Xbox controller optimized for our drive team.
 
 public class SnailController extends XboxController {
-	double [] Deadbanded;
+	double z;
 
 	public SnailController(int port) {
+		z=0;
 		super(port);
 	}
 
@@ -47,16 +48,25 @@ public class SnailController extends XboxController {
 		else
 			return 0;
     }
-    @Override
-    public double[] applyDeadBand(double x, double y){ // scaled radial 
+    
+    public double applyDeadBandDrive(double x, double y){ // scaled radial 
         z = Math.pow(Math.pow(x, 2)+Math.pow(y, 2),0.5); //magnitude of vector
         if( z < RobotMap.Neo_DeadBand){  
-            double[] Deadbanded = new double[]{0,0};
-            return Deadbanded;
+            return 0;
+        }
+        else{ 
+            return x*(z-Robot._Neo_DeadBand)/z;
+        }
+
+    }
+	public double applyDeadBandTurn(double x, double y){ // scaled radial 
+        z = Math.pow(Math.pow(x, 2)+Math.pow(y, 2),0.5); //magnitude of vector
+        if( z < RobotMap.Neo_DeadBand){  
+            return 0;
         }
         else{
             double [] Deadbanded = new double[]{x*(z-Robot._Neo_DeadBand)/z,y*(z-Robot._Neo_DeadBand)/z}; 
-            return Deadbanded;
+            return y*(z-Robot._Neo_DeadBand)/z;
         }
 
     }
