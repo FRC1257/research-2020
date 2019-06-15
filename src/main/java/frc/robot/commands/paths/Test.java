e7477c328272764464f8772c60775249389df959
ryan10145 <chang.ryan1@yahoo.com>
2019-06-15 12:23:08 -0400

    Fix Build Error

--- src/main/java/frc/robot/commands/paths/Test.java
+++ src/main/java/frc/robot/commands/paths/Test.java
@@ -3,8 +3,8 @@ package frc.robot.commands.paths;
 import easypath.FollowPath;
 import easypath.Path;
 
-public class RightMiddleHatch extends FollowPath {
-    public RightMiddleHatch() {
+public class Test extends FollowPath {
+    public Test() {
         super(new Path(t -> 
                 /* {"start":{"x":50,"y":164},"mid1":{"x":91,"y":160},"mid2":{"x":154,"y":176},"end":{"x":155,"y":140}} */
                 (-216 * Math.pow(t, 2) + 120 * t + -12) / (-252 * Math.pow(t, 2) + 132 * t + 123),
